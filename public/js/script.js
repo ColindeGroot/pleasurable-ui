65e1650cf4a185ba8ecb2f695caabe76047c543f
Colin de Groot <colindegroot2004@gmail.com>
2025-05-22 23:56:49 +0200

    standaard html en css af close #34
    laatste css en containers voor styling toegevoegd. nu kunnen we aan de animaties!!!!

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,14 @@
-s
+    // zet de focus gelijk op de popup wanneer deze word geopend. Dit is een enchanment voor o.a. screenreaders zodat ze weten wat ze moeten voorlezen
+    // ik ben hier nog beetje mee aan het spelen om op de goede plek de focus te leggen
+
+    const dialog = document.getElementById('my-popover');
+    const openButton = document.querySelector('[popovertarget="my-popover"]');
+
+    dialog.addEventListener('toggle', () => {
+        if (dialog.open) {
+            // Focus op container zelf, zodat screenreader de naam (titel) voorleest
+            dialog.focus();
+        } else {
+            openButton.focus();
+        }
+    });
