beadc0b97b6ff00ca219fd31d025c747ad4bdf80
Colin de Groot <colindegroot2004@gmail.com>
2025-05-27 14:58:52 +0200

    open button toegevoegd met aniamtie en client side js voor extra toegangkelijkheid en enhanchment  close #59  close #25

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,38 @@
-    // zet de focus gelijk op de popup wanneer deze word geopend. Dit is een enchanment voor o.a. screenreaders zodat ze weten wat ze moeten voorlezen
-    // ik ben hier nog beetje mee aan het spelen om op de goede plek de focus te leggen
+// // zet de focus gelijk op de popup wanneer deze word geopend. Dit is een enchanment voor o.a. screenreaders zodat ze weten wat ze moeten voorlezen
+// // ik ben hier nog beetje mee aan het spelen om op de goede plek de focus te leggen lukt nog niet helemaal
 
-    const dialog = document.getElementById('my-popover');
-    const openButton = document.querySelector('[popovertarget="my-popover"]');
+// const dialog = document.getElementById('my-popover');
+// const openButton = document.querySelector('[popovertarget="my-popover"]');
 
-    dialog.addEventListener('toggle', () => {
-        if (dialog.open) {
-            // Focus op container zelf, zodat screenreader de naam (titel) voorleest
-            dialog.focus();
-        } else {
-            openButton.focus();
-        }
+// dialog.addEventListener('toggle', () => {
+//     if (dialog.open) {
+//         // Focus op container zelf, zodat screenreader de naam (titel) voorleest
+//         dialog.focus();
+//     } else {
+//         openButton.focus();
+//     }
+// });
+
+// voor browsers die geen popover ondersteunen
+
+const dialog = document.getElementById('my-popover');
+  const openBtn = document.getElementById('open-popup');
+
+  const supportsPopoverOpen = CSS.supports('selector(:popover-open)');
+
+  if (!supportsPopoverOpen) {
+    const closeBtn = document.querySelector('[popovertarget="my-popover"]');
+
+    if (closeBtn) {
+      closeBtn.addEventListener('click', () => {
+        dialog.classList.add('closed');
+      });
+    }
+
+    openBtn.addEventListener('click', () => {
+      dialog.classList.remove('closed');
     });
 
-    
+    // Zorg dat de juiste status actief is bij paginalaad
+    dialog.classList.add('closed');
+  }
